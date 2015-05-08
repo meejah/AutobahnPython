ae512063cc9884f94ff77ef8e4c2fc3b7014976c
meejah <meejah@meejah.ca>
2015-05-08 15:54:49 -0600

    change all JS URIs to be public router by default

--- examples/twisted/wamp/basic/pubsub/complex/backend.js
+++ examples/twisted/wamp/basic/pubsub/complex/backend.js
@@ -6,8 +6,8 @@ try {
 }
 
 var connection = new autobahn.Connection({
-   url: 'ws://127.0.0.1:8080/ws',
-   realm: 'realm1'}
+   url: 'wss://demo.crossbar.io/ws',
+   realm: 'crossbardemo'}
 );
 
 function randint(min, max) {
