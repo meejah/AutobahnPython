08c751ef8644caf85363be1355e60d93712401ca
meejah <meejah@meejah.ca>
2015-07-08 13:21:06 -0600

    expand example

--- examples/twisted/wamp/unix-rawsocket/frontend.py
+++ examples/twisted/wamp/unix-rawsocket/frontend.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import random
+
 from twisted.internet.defer import inlineCallbacks, DeferredList, Deferred
 from twisted.internet.task import react
 
@@ -52,6 +54,16 @@ def main(reactor):
         }
     }
 
+    websocket_tcp_transport = {
+        "type": "websocket",
+        "url": "ws://127.0.0.1/ws",
+        "endpoint": {
+            "type": "tcp",
+            "host": "127.0.0.1",
+            "port": 8081,
+        }
+    }
+
     retry = dict(
         initial_retry_delay=1,
         retry_growth_rate=2,
@@ -59,14 +71,37 @@ def main(reactor):
         retry_on_unreachable=True,
     )
 
+    transports = [bad_transport, rawsocket_unix_transport, websocket_tcp_transport, {"just": "completely bogus"}]
+    def random_transports():
+        while True:
+            t = random.choice(transports)
+            # print("Returning transport:", t)
+            yield t
+
     if False: # retry
         connection = yield runner.run(ClientSession, start_reactor=False, retry=retry)
         runner = ApplicationRunner([bad_transport, bad_transport, rawsocket_unix_transport],#, bad_transport, bad_transport],
                                    u"realm1")
     else:
-        runner = ApplicationRunner([rawsocket_unix_transport], u"realm1")
+        # single, good unix transport
+        #runner = ApplicationRunner([rawsocket_unix_transport], u"realm1")
+
+        # single, good tcp+websocket transport
+        #runner = ApplicationRunner([websocket_tcp_transport], u"realm1")
+
+        # single, bad transport (will never succeed)
         #runner = ApplicationRunner([bad_transport], u"realm1")
+
+        # use generator/iterable as infinite transport list
+        runner = ApplicationRunner(random_transports(), u"realm1")
+
+        # hmm or should we just return None from run() and access connection
+        # via runner.connection always?
+
         connection = yield runner.run(ClientSession, start_reactor=False)
+        assert connection is runner.connection
+
+    # "advanced" usage, passing "start_reactor=False" so we get access to the connection object
     print("Connection!", connection)
     connection.add_event(Connection.CREATE_SESSION, lambda s: print("new session:", s))
     connection.add_event(Connection.SESSION_LEAVE, lambda s: print("session gone:", s))
@@ -74,13 +109,15 @@ def main(reactor):
     connection.add_event(Connection.ERROR, lambda e: print("connection error:", e))
 
     def shutdown(reason):
-        print("shutdown", reason)
-        reactor.stop()
+        print("shutdown because '{}'".format(reason))
+        #reactor.stop()
     connection.add_event(Connection.CLOSED, shutdown)
 
     while True:
         yield sleep(1)
         print("connection:", connection)
+        if connection.session:
+            connection.session.publish('foo')
     print("exiting main")
 
 react(main)
