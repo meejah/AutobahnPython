a7cef320dd019a82bf6d31bc20471ee1d5ef23eb
meejah <meejah@meejah.ca>
2016-12-01 19:43:52 -0700

    better example

--- examples/twisted/wamp/work/newapi/func_component.py
+++ examples/twisted/wamp/work/newapi/func_component.py
@@ -4,14 +4,27 @@
 # configured as "type": "function"
 #
 
+from twisted.internet.defer import inlineCallbacks
+from autobahn.twisted.util import sleep
+
+@inlineCallbacks
 def on_join(session, details):
     print("functional join", session, details)
+    delay = 5
+    while delay > 0:
+        print("leaving in {}s".format(delay))
+        yield sleep(1)
+        delay -= 1
+    yield session.leave()
+
 
 def on_leave(session, details):
     print("functional leave", session, details)
 
+
 def on_connect(session, transport):
     print("functional connect", session, transport)
+
 
 def on_disconnect(session):
     print("functional disconnect", session)
