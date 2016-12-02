2bd24f31b50dfcf7d5cbac8cf966b9f083030a62
meejah <meejah@meejah.ca>
2016-12-01 17:58:06 -0700

    remove debug

--- autobahn/wamp/protocol.py
+++ autobahn/wamp/protocol.py
@@ -440,7 +440,6 @@ class ApplicationSession(BaseSession):
         Implements :func:`autobahn.wamp.interfaces.ITransportHandler.onMessage`
         """
 
-        print("got message", msg, self._session_id)
         if self._session_id is None:
 
             # the first message must be WELCOME, ABORT or CHALLENGE ..
@@ -467,9 +466,7 @@ class ApplicationSession(BaseSession):
                 # including self.leave() -- works properly. Besides,
                 # there's "ready" that fires after 'join' and onJoin
                 # have all completed...
-                print("firing join", details)
                 d = self.fire('join', self, details)
-                print("fired", d)
                 # add a logging errback first, which will ignore any
                 # errors from fire()
                 txaio.add_callbacks(
@@ -564,7 +561,6 @@ class ApplicationSession(BaseSession):
         else:
             # self._session_id != None (aka "session established")
             if isinstance(msg, message.Goodbye):
-                print("GOT GOODBYE")
                 if not self._goodbye_sent:
                     # the peer wants to close: send GOODBYE reply
                     reply = message.Goodbye()
