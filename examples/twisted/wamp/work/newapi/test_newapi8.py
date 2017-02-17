528635e76e43c45be2523bbd72ef09cae3a8ae5f
meejah <meejah@meejah.ca>
2017-02-16 18:48:11 -0700

    rewrite example to do the same thing in new-new API

--- examples/twisted/wamp/work/newapi/test_newapi8.py
+++ examples/twisted/wamp/work/newapi/test_newapi8.py
@@ -1,6 +1,8 @@
-from twisted.internet.task import react
+from twisted.internet.task import react, deferLater
+from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks as coroutine
-from autobahn.twisted.wamp import Connection
+from autobahn.twisted.component import Component, run
+from autobahn.twisted.wamp import Session
 
 # A single session joins a first realm, leaves and joins another realm
 # all over the same, still running transport
@@ -11,33 +13,42 @@ from autobahn.twisted.wamp import Connection
 # def _(session, details):
 #     yield session.leave()
 #     yield session.join(u'another_realm')
+# okay, so the default implementation of ApplicationSession overrides
+# leave() to do a disconnect .. so we have to override that.
 
+class _NoDisconnect(Session):
+    def onLeave(self, details):
+        pass
+
+
+component = Component(realm=u'crossbardemo')
+component.session_factory = _NoDisconnect
+
+
+@component.on_join
 @coroutine
-def main(transport):
-    session = ApplicationSession()
-
-    # join a first realm and do something
-    yield session.join(transport, u'myrealm1')
-    result = yield session.call(u'com.myapp.add2', 2, 3)
-    print("Result: {}".format(result))
-
-    # leave the realm. the transport will NOT be closed!
-    yield session.leave()
-
-    # join a different realm and do something
-    yield session.join(transport, u'myrealm2')
-    result = yield session.call(u'com.foobar.mul2', 2, 3)
-    print("Result: {}".format(result))
-
-    # leave the realm. the transport will NOT be closed!
-    yield session.leave()
-
-    # now close the transport. after this, the transport cannot
-    # be reused!
-    yield transport.close()
+def on_join(session, details):
+    print("joined: {}".format(details))
+    if details.realm != u'another_realm':
+        print("switching realms")
+        session.leave()
+        print("Left; waiting 2 seconds")
+        yield deferLater(reactor, 2, lambda: None)
+        print("joining 'another_realm'")
+        yield session.join(u'another_realm')
+    else:
+        print("waiting 5 seconds")
+        for x in range(5):
+            print("{}...".format(x + 1))
+            yield deferLater(reactor, 1, lambda: None)
+        print("leaving")
+        session.leave()
+        print("Disconnectin in 1 second")
+        yield deferLater(reactor, 1, lambda: None)
+        print("disconnecting")
+        yield session.disconnect()
 
 
 
 if __name__ == '__main__':
-    connection = Connection(main)
-    react(connection.start)
+    run(component, 'info')
